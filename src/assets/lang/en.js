8a14bbbaec69352d36205c957599be1cb7cc78fd
Lerddee <deelerdkub@gmail.com>
2021-04-15 12:32:59 +0700

    lang

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -1,5 +1,11 @@
 const lang = {
   locale: 'en',
+  menu: 'Menu',
+  first_name: 'Lerddee',
+  last_name: 'Vivachephisarlkul',
+  contacts: 'Contacts',
+  profile: 'Profile',
+  coding: 'Coding',
 };
 
 export default lang;
--- src/assets/lang/th.js
+++ src/assets/lang/th.js
@@ -1,5 +1,11 @@
 const lang = {
   locale: 'th',
+  menu: 'เมนู',
+  first_name: 'เลิศดี',
+  last_name: 'วิเวชไพศาลกุล',
+  contacts: 'ข้อมูลติดต่อ',
+  profile: 'โปรไฟล์',
+  coding: 'การเขียนโค้ด',
 };
 
 export default lang;
