b4c4c22b987bddac4b77c79d02ae366509f702b1
Lerddee <deelerdkub@gmail.com>
2021-04-15 12:32:00 +0700

    default language

--- src/redux/reducers/language.js
+++ src/redux/reducers/language.js
@@ -4,20 +4,20 @@ import { getCookies, setCookies } from '../../libs/cookies'
 
 const options = { expires: 2 * 365 }
 
-let defaultLocale = getCookies('__lang') || 'th'
+let defaultLocale = getCookies('__lang') || 'en'
 
 let initState = {
-    locale: getCookies('__lang') || 'th',
+    locale: defaultLocale,
     dictionary: dictionary[defaultLocale]
 }
 
 const changeLang = (state = initState, action) => {
     switch (action.type) {
         case SELECT_LANGUAGE:
-            setCookies('__lang', action.data, options)
+            setCookies('__lang', action.data || defaultLocale, options)
             return Object.assign({}, state, {
-                locale: action.data,
-                dictionary: dictionary[action.data]
+                locale: action.data || defaultLocale,
+                dictionary: dictionary[action.data || defaultLocale]
             })
         default:
             return state
