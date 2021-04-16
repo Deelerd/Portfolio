04ea82defd8833d45c3c3c0d9364ba40c064ce0e
Lerddee <deelerdkub@gmail.com>
2021-04-16 16:21:18 +0700

    moment language

--- src/redux/reducers/language.js
+++ src/redux/reducers/language.js
@@ -1,6 +1,8 @@
 import dictionary from '../../assets/lang'
 import { SELECT_LANGUAGE } from '../action_const'
 import { getCookies, setCookies } from '../../libs/cookies'
+import moment from 'moment'
+import 'moment/locale/th'
 
 const options = { expires: 2 * 365 }
 
@@ -15,6 +17,7 @@ const changeLang = (state = initState, action) => {
     switch (action.type) {
         case SELECT_LANGUAGE:
             setCookies('__lang', action.data || defaultLocale, options)
+            moment.locale(action.data || defaultLocale)
             return Object.assign({}, state, {
                 locale: action.data || defaultLocale,
                 dictionary: dictionary[action.data || defaultLocale]
