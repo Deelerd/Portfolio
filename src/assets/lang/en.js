bf8481cc1e1b70fa03c46f55e22a61ba5bf97395
Lerddee <deelerdkub@gmail.com>
2021-04-16 16:23:19 +0700

    language

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -6,6 +6,142 @@ const lang = {
   contacts: 'Contacts',
   profile: 'Profile',
   coding: 'Coding',
+  year: 'Year',
+
+  // profile
+  personal: 'Personal Profile',
+  my_birthday: 'December 21, 1996',
+  my_phone: '0845428595',
+  my_email: 'deelerdkub@gmail.com',
+  my_address: 'No.70, Soi Nakniwat 21 Yaek 2-3, Nakniwat Road, Ladphrao District, Bangkok, 10230',
+  interests_and_hobbies: 'Interests and Hobbies',
+  computer: 'Computer',
+  book: 'Book',
+  music: 'Music',
+  camera: 'Photograph',
+  badminton: 'Badminton',
+  travel: 'Travel',
+  education: 'Education',
+  my_education: `King Mongkut’s Institute of Technology Ladkrabang`,
+  my_bachelor: 'Bachelor of Engineering - Computer Engineering',
+  my_gpa: 'GPA : 3.27',
+  skills: 'Skills',
+  objective: 'Objective',
+  my_objective: [
+    'Enhance knowledge and ability to become a busines owner',
+    'Apply existing knowledge and capabilities to maximize benefits'
+  ],
+  qualification: 'Qualification',
+  my_qualification: [
+    'Dependable, responsible and self motivated',
+    'Openmind, enthusiastic to learn a newthing',
+    'Be punctual and have perseverance',
+    'Be creative',
+  ],
+  work_experience: 'Work experience',
+  total_experience: 'Total experience',
+  title: 'Title',
+  position: 'Position',
+  duration: 'Duration',
+  company_description : 'Company description',
+  project: 'Project',
+  project_description: 'Project description',
+  responsibility: 'Responsibility',
+  success: 'Success',
+  reference: 'Reference',
+  links: 'Links',
+  my_experience: [{
+    company_name: 'ReadyPlanet',
+    position: 'Software developer',
+    start_date: '2019-06-04',
+    end_date: '2021-04-30',
+    company_description: `ReadyPlanet is all-in-one sales and marketing platform provider with a team of 150+ experts.
+    The platform and digital advertising that has been certified and be the first Google partner in Thailand.
+    The company are committed to helping SMEs and enterprises in Thailand succeed with over 20 years of experience since 2000.
+    Today, ReadyPlanet is trusted by leading entrepreneurs, companies, associations and organizations in Thailand over 15,000+ businesses 
+    to use platform to build web, web design, mobile app, CRM system and more.`,
+    project: 'PointSpot',
+    project_description: `The points collection system by phone number that help to boost sales and increase repeat purchase and let customers come back to use.
+    The program has function that easy to use such as send points to customers, manage member, manage coupons and rewards, and view statistics and reports.`,
+    responsibility: [
+      'Develop a web application about Points Colection System (PointSpot)(Develop features,improve performance,fixbugs and make a reports)',
+      'Plan design and discuss with the team for effective development',
+      'Develope frontend by HTML/CSS and ReactJS/Redux',
+      'Develope backend by NodeJS and manage data by PostgreSQL',
+      'Reporting with Grafana'
+    ],
+    success: [
+      'Learned the process of working and team work',
+      'Learned new coding skills',
+      'Enhance a planning, thinking and decision skill',
+      'The project have greater functionality and efficiency.'
+    ],
+    reference: [{ name: 'Phisit Kannasut', contact: '0636695587' }],
+    link_company: ['https://www.readyplanet.com', 'https://th.pointspot.co']
+  }, {
+    company_name: 'Thesis',
+    position: '',
+    start_date: '2018-08-01',
+    end_date: '2019-04-30',
+    company_description: `King Mongkut’s Institute of Technology Ladkrabang`,
+    project: 'Smart Electronic Auction Application',
+    project_description: `The objective of this project is to create a facilitated application for online auction and price offering. 
+    At the present time, there are many people who trade via online channels. 
+    Auction is one of the most popular form of trading. The users can sell their own commodity by auction and price offering form.
+    There are many convenient functions that are easy to use and facilitate both buyers and sellers,
+    for example, an automatic category classification system that classifies by a product image,
+    an augment reality try on that the user can try on some products in a reality view,
+    recommendation system that users will be recommended product in the same category by reference from user behavior and
+    animation preview function that the sellers can preview their product in various angle automatically.`,
+    responsibility: [
+      'Learn and study about Machine learning and Deep learning',
+      'Learn and study about Convolutional Neural Network',
+      'Make an automatic category classification system that classifies by a product image. Take pictures to teach the model and test it.',
+      `Make a recommendation system that uses a method to analyze the user's face from a photograph with Microsoft Azure,
+       After receiving the results from the face analysis such as gender, age, wearable (Hat, glasses) hair color, make-up
+       The application will use the information to recommend the product to the user with specified conditions.`,
+      'Make a thesis and present'
+    ],
+    success: [
+      'Received Grade A',
+      'Project can be base for the younger generation to take on'
+    ],
+    reference: [{ name: 'Asst.Prof.Dr.Pikulkaew Tangtisanont' }],
+    link_company: []
+  }, {
+    company_name: 'Internship',
+    position: 'Data Engineering',
+    start_date: '2018-06-04',
+    end_date: '2018-08-03',
+    company_description: `KASIKORN Business Technology Group`,
+    project: 'Product Offering - Mutual Fund',
+    project_description: `Analyze the customers data to effectively offer the product to customer`,
+    responsibility: [
+      `Learn and study about functions and processes of work including required software from expert and speakers.`,
+      `Design ideas about the company topic, What to do, how to do it for someone, what problems can be solved, what are the benefits ?`,
+      `Explore and visualize the data that provided by the company by query with SQL language on impala via HUE.`,
+      `Make a data visualization to see trends and to learn the data through graphs by writing python in jupyter.`,
+      `Learn more about the python language and machine learning to understand and effectively usage.`,
+      `Create a model to predict customers who are likely to buy funds and prepare presentation.`,
+      'Pitching races with other groups'
+    ],
+    success: [
+      'Received a teamwork award from pitching',
+      `Learned team work and coordination with team.`,
+      `Learned to plan and scope of work from start to finish.`,
+      `Learned to manage working time`,
+      `Learned theories that did not know and Learned to solve the problems.`,
+      `Have greater responsibility and cautious.`,
+      `Learned about principles of good presentation`
+    ],
+    reference: [],
+    link_company: []
+  }],
+  job_interests: 'Job interests',
+  my_job_interests: [
+    'Full stack developer',
+    'Frontend developer'
+  ]
 };
 
 export default lang;
